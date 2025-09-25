1db3b549a3bbb7af23bb03ccea7ccbfbec2a3a76
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-25 15:07:36 -0300

    adicionando validacao de campos

--- Crud-MVC/Controllers/SellersController.cs
+++ Crud-MVC/Controllers/SellersController.cs
@@ -33,6 +33,13 @@ namespace Crud_MVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -81,6 +88,13 @@ namespace Crud_MVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
                 return RedirectToAction(nameof(Error), new { Message = "Id mismatch" }); ;
 
