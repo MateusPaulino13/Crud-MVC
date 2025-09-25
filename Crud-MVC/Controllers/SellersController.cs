7b2843d530204a7dd8d3f57d445e8bacfbb38d5e
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-25 12:34:02 -0300

    adicionando funcao delete Seller

--- Crud-MVC/Controllers/SellersController.cs
+++ Crud-MVC/Controllers/SellersController.cs
@@ -34,5 +34,23 @@ namespace Crud_MVC.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            var seller = _sellerService.FindById(id.Value);
+
+            if (id == null || seller == null)
+                return NotFound();
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
